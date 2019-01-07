82b077762b61ac8c695855846ccf95f9a3de7fec
Kostya Yatsuk <kostya.yatsuk9@live.com>
2019-01-07 13:21:24 -0800

    Chipping away at the new CowBot

--- Halite III/CowBot.py
+++ Halite III/CowBot.py
@@ -24,12 +24,37 @@ game.ready("MyPythonBot")
 logging.info("Successfully created bot! My Player ID is {}.".format(game.my_id))
 
 
+""" <<<Constants>>> """
+
+MAX_REMAINING_HALITE = 50
+
 """ <<<Functions>>> """
 # Define all the functions used in this bot
 
 def findTotalHalite(game_map):
     # Iterate over all map cells and calculate the total halite
+    total_halite = 0
+    for row in game_map.height:
+        for col in game_map.width:
+            pos = Position(row, col)
+            total_halite += game_map[pos].halite_amount
+    return total_halite
 
+
+def isStationary(ship, game_map):
+    # Determines if a ship is going to stay still this turn
+    # Only moves if the ship is full or the position it is at has no more halite to farm
+    if ship.is_full:
+        return False
+    elif game_map[ship.position].halite_amount <= MAX_REMAINING_HALITE:
+        return False
+    else:
+        return True
+
+
+def selectMove(taken_pos, ship, game_map, me):
+    # Determines best move for a ship to take to get to either the shipyard 
+    # or to the square with the most halite
 
 """ <<<Game Loop>>> """
 
@@ -51,11 +76,11 @@ while True:
 
     # Filter out the stationary ships first to handle collision avoidance
     for ship in all_ships:
-        # If the ship remains stationary:
-        #   remove it from all_ship
-        #   add its position to taken_pos
-        #   command_queue.append(ship.stay_still())
-        #   add how much halite it will collect to collected_total
+        if isStationary(ship, game_map):
+            all_ships.remove(ship)
+            taken_pos.append(ship.position)
+            command_queue.append(ship.stay_still())
+            collected_total += game_map[ship.position].halite_amount * 0.25
     
     # Find new moves for all the ships which will be moving this turn
     for ship in all_ships:
@@ -63,6 +88,8 @@ while True:
         # Add that position to taken_pos
         # Append the new move to command_queue
     
+    percent_collected = total_halite / collected_total
+
     # If the percent collected per turn is less than a given percentage and you have enough halite, spawn a ship.
     # Don't spawn a ship if you currently have a ship at port.
     if percent_collected < 0.10 and me.halite_amount >= constants.SHIP_COST and not game_map[me.shipyard].is_occupied:
